62386c6d19a77b3380eab4475a5124aefface8e3
Aimen <aimen.mk07@gmail.com>
2026-06-13 19:04:48 +0400

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
+# Simple Calculator Project
 
+print("=== SIMPLE CALCULATOR ===")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operation = input("Choose operation (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Cannot divide by zero"
+else:
+    result = "Invalid operation"
+
+print("Result:", result)
